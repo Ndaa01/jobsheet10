4df6ce42312892d3e4f0a70f3a0ef74c7f9700da
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-30 13:11:15 +0700

    Second

--- Bioskop05.java
+++ Bioskop05.java
@@ -2,9 +2,9 @@ public class Bioskop05 {
     public static void main(String[] args) {
         String [] [] penonton = new String[4][2]; 
         
-        penonton [1][0] = "Amin";
+        penonton [0][0] = "Amin";
         penonton [0][1] = "Bela";
-        penonton [0][0] = "Candra";
+        penonton [1][0] = "Candra";
         penonton [1][1] = "Dela";
         penonton [2][0] = "Eka";
         penonton [2][1] = "Farhan";
@@ -19,8 +19,8 @@ public class Bioskop05 {
 
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris : " + barisPenonton.length);
         }
 
 
